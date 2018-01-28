80b02472c5e3e21e87ac322ac16cf115cb90d338
Sukhmeet Khalar <mithusingh32@gmail.com>
2018-01-28 11:34:47 -0500

    More streamlined and added local log file
    Created variables for better readiability.
    Added: Writes a log file to keep track of IP addres changes

--- email_publicip.py
+++ email_publicip.py
@@ -1,23 +1,37 @@
 import email
 import smtplib
 from urllib2 import urlopen
-import time
 from datetime import date
+import os.path
 
-my_ip = urlopen('http://ip.42.pl/raw').read()
+SMTPServer = ""  # Enter the smtp server
+SMTPPort = 000  # Enter smtp server port number
+myEmail = ""  # Email you wish to be sender
+targetEmail= ""  # Email that is the target
+my_ip = urlopen('http://ip.42.pl/raw').read()  # Retrieves external IP, you can change source
 
-
-msg = email.message_from_string("IP adress for "+ str(date.today()) + " is: "+ my_ip)
+# Creates email object
+msg = email.message_from_string("IP adress for " + str(date.today()) + " is: " + my_ip)
 msg['From'] = "From"
 msg['To'] = "To"
 msg['Subject'] = "IP Address"
 
-server = smtplib.SMTP("smtp.live.com",587)
-server.ehlo() # Hostname to send for this command defaults to the fully qualified domain name of the local host.
-server.starttls() #Puts connection to SMTP server in TLS mode
+server = smtplib.SMTP(SMTPServer, SMTPPort)
+server.ehlo()  # Hostname to send for this command defaults to the fully qualified domain name of the local host.
+server.starttls()  # Puts connection to SMTP server in TLS mode
 server.ehlo()
-server.login('email address', 'password')
+server.login(myEmail, "Enter email pass")  # TODO Create safer way for this information to be input
 
-server.sendmail("your email address again", ["Who you're sending to"], msg.as_string())
-
+server.sendmail(myEmail, [targetEmail], msg.as_string())
 server.quit()
+
+# Creates a txt log file for local storage
+# Recommend directing file to a cloud location or run script from cloud
+if os.path.exists("log.txt"):
+    logFile = open("log.txt", "a")
+    logFile.write("\n[" + str(date.today()) + "] " + my_ip)
+    logFile.close()
+else:
+    logFile = open("log.txt", "w")
+    logFile.write("\n[" + str(date.today()) + "] " + my_ip)
+    logFile.close()
